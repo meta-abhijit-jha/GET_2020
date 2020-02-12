5a8ee15c900b06fdd0ade57774c142e7a5e62175
Abhijit Jha <abhijit.jha@metacube.com>
2020-02-12 13:20:57 +0530

    Updated Soap Assignment

--- GET_2020/EAD_6_2/soap/src/com/soapWebServices/AreaTriangle.java
+++ GET_2020/EAD_6_2/soap/src/com/soapWebServices/AreaTriangle.java
@@ -3,9 +3,9 @@ package com.soapWebServices;
 public class AreaTriangle {
 	public double area(double sideA, double sideB, double sideC) {
 		try {
-			if((sideA > 0) && (sideB > 0) && (sideC > 0)) {
+			if((sideA > 0) && (sideB > 0) && (sideC > 0) && ((sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA))) {
 				double s = (sideA + sideB + sideC)/2;
-				double areaTriangle = s * (s - sideA) * (s - sideB) * (s - sideC);
+				double areaTriangle = Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
 				return areaTriangle;
 			} else {
 				System.out.println("Invalid Input");
